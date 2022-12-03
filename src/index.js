1b898c02af1583cb93eeb98bafee71134923509b
djk01281 <djk01281@gmail.com>
2022-12-03 06:03:51 +0000

    chore: setup firebase

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("hello from index.js");
+import { inintializeApp, initializeApp } from "firebase/app";
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyATCD9cxXSJ2RSUwd9WSDHTDMV7FuSaCgo",
+  authDomain: "fir-demo-48710.firebaseapp.com",
+  projectId: "fir-demo-48710",
+  storageBucket: "fir-demo-48710.appspot.com",
+  messagingSenderId: "49933278590",
+  appId: "1:49933278590:web:bb643d3e5d7b2077cd672f",
+  measurementId: "G-HFF9Z86BJM",
+};
+
+initializeApp(firebaseConfig);
